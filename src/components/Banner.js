7924f6af63473e34cedaa07a353b0b5114aabdbf
Priscila Ribeiro <priscila.criol@gmail.com>
2022-11-15 02:20:48 -0300

    format document

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 const StyledBanner = styled.div`
   position: relative;
-display: flex;
-height: 230px;
-left: 0px;
-top: 56px;
-  background: url("https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop");
+  display: flex;
+  height: 230px;
+  left: 0px;
+  top: 56px;
+  background-image: url("https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop");
 `;
 
 export default function Banner() {
